dfb859d4783313eac9ea6df940dbe4b0dc54a53e
Saukipun <89566397+Saukipun@users.noreply.github.com>
2021-08-26 13:03:28 +0700

    Add files via upload

---
+++ ASP.Net Core API Sauki/viewModels/vBackendQuestionOneResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Net_Core_API_Sauki.viewModels
+{
+    public class vBackendQuestionOneResponse
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string footer { get; set; }
+        public DateTime createdAt { get; set; }
+    }
+}
